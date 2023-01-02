d83e0edef00b7909e7ed20c9cfba409b4010ea40
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-02 14:26:08 -0700

    added basic styling to form compenents, set up form with inputs, button and select menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import Tricks from './Tricks'
 import Form from './Form'
 
-// Get all existing tricks from the API on page load and display them on the DOM.
+// Create a controlled Form component to create a trick 
+// (look into the API documentation to see what pieces of information are expected,
+// and what datatypes it wants each piece to be). As a user types into the input fields, 
+// the form component's state should change. When the user clicks the SEND IT button, 
+// the application should update the App's state. The new trick should display with 
+// all of the existing tricks.
 
 class App extends Component {
   constructor() {
--- src/components/App/Form.js
+++ src/components/App/Form.js
@@ -2,18 +2,48 @@ import React from "react";
 import { Component } from "react";
 import './Form.css'
 
-class Form extends Component{
-    constructor(){
-        super()
-        this.state={
+// { stance: <String>, name: <String>, obstacle: <String>', tutorial: <String>}
 
+class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+            stance: '',
+            name: '',
+            obstacle: '',
+            tutorial: ''
         }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <form>
-                <h1>Form here</h1>
+                <select name="trickStance" id="trickStance">
+                    <option value="...">Choose your Stance</option>
+                    <option value="regular">regular</option>
+                    <option value="switch">switch</option>
+                </select>
+                <input
+                    type='text'
+                    placeholder='Name of Trick'
+                    name='trickName'
+                    value={this.state.name}
+                />
+                <select name="trickObstacle" id="trickObstacle">
+                <option value="...">Choose your Obstacle</option>
+                    <option value="flatground">flatground</option>
+                    <option value="ledge">ledge</option>
+                    <option value="rail">rail</option>
+                    <option value="stairs">stairs</option>
+                    <option value="pool">pool</option>
+                </select>
+                <input
+                    type='text'
+                    placeholder='Link to Tutorial'
+                    name='trickTutorial'
+                    value={this.state.tutorial}
+                />
+                <button>Send It!</button>
             </form>
         )
     }
