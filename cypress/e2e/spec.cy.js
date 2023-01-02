388c844216f191bc1d690e27708f3947c4935cd9
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-02 15:59:11 -0700

    refactor: reorganized cypress tests into modular style, removed any comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,7 +7,7 @@ describe('Visit site', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('Should verify display of title, stance menu, name textbox, obstacle menu, link texbox, submit button, and 1 trick card', () => {
+  it('Should display page upon load (title, form fields, button, & 1 trick card)', () => {
     cy.get('h1').contains('Sick Trick Wish List')
     cy.get('#trickStance').contains('Choose your Stance')
     cy.get('#name').should('have.attr', 'placeholder', 'Name of Trick')
@@ -21,7 +21,7 @@ describe('Visit site', () => {
     cy.get('a').contains('https://www.youtube.com/watch?v=XGw3YkQmNig')
   })
 
-  it('Should take inputs from user, update inputs, and generate a new trick card', () => {
+  it('Should take inputs from user and show updated inputs in each field', () => {
     cy.get('#trickStance').select('regular')
     cy.get('#trickStance').contains('regular')
 
@@ -33,6 +33,13 @@ describe('Visit site', () => {
 
     cy.get('#tutorial').type('youtube.com')
     cy.get('#tutorial').should('have.value', 'youtube.com')
+  })
+
+  it('Should generate a new trick card when user fills out form', () => {
+    cy.get('#trickStance').select('regular')
+    cy.get('#name').type('cool doggo')    
+    cy.get('#trickObstacle').select('flatground')
+    cy.get('#tutorial').type('youtube.com')
 
     cy.get('button').click()
 
--- src/components/App/Form.js
+++ src/components/App/Form.js
@@ -1,4 +1,3 @@
-import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 import { Component } from "react";
 import './Form.css'
