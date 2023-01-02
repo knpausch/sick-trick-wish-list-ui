43b3db8f1b91f097bebd908e0c55a173f419cd6b
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-02 13:34:20 -0700

    Established constructor, fetched API upon component did mount function and stored trick data to state trickList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,37 @@
 import { Component } from 'react';
 import './App.css';
 
+// Get all existing tricks from the API on page load and display them on the DOM.
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      trickList: []
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
       </div>
-    );
+    )
+  }
+
+  getTricks = () => {
+    fetch('http://localhost:3001/api/v1/tricks')
+    .then(response => response.json())
+    .then(data => {
+      console.log("DATA: ", data)
+      this.setState({trickList: data})
+      return
+    })
+  }
+
+  componentDidMount = () => {
+    this.getTricks()
   }
 }
 
